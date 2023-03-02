a6917130fe8318b91197824f2d7328aa87ac50d5
ariel <arieltwain@gmail.com>
2023-03-01 21:01:05 -0300

    working with api http post request - methods stringbody, elfilestring

--- src/test/scala/basicpackage/StringBody_Post_Assignment.scala
+++ src/test/scala/basicpackage/StringBody_Post_Assignment.scala
@@ -3,14 +3,14 @@ package basicpackage
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class RawFileBody_Post extends Simulation {
+class StringBody_Post_Assignment extends Simulation {
 
   val httpConf = http.baseUrl("https://api.rebrandly.com/").header("apikey", "2a9b90cf3cee486fa4138e9d8434d077")
 
   val scn = scenario("CreateLink")
       .exec(http("CreateLinkRequest")
         .post("/v1/links")
-          .body(RawFileBody("bodies/myFile.json")).asJson)
+          .body(StringBody("""{"destination": "https://github.com/ArielBandeira/learningGatling", "slashtag": "newPosttag1", "title": "QA Milestone Post"}""")).asJson)
 
   setUp(scn.inject(atOnceUsers(1))).protocols(httpConf)
 
